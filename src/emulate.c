bcddf4ef6c3d467909cb567a50d1bd22cda81a84
Om Singh <os221@ic.ac.uk>
2022-05-31 19:55:04 +0100

    Polished Reading files

--- src/emulate.c
+++ src/emulate.c
@@ -4,18 +4,19 @@
 #include <string.h>
 
 #define MEMSIZE 64 * 1024
+#define NOREGS 17
 
 typedef uint8_t byte;
 typedef uint32_t word;
 
 typedef struct state {
   byte memory[MEMSIZE];
-  word registers[17];
+  word registers[NOREGS];
 } state_t;
 
 void init_emulator(state_t *s) {
   memset(s->memory, 0, MEMSIZE * sizeof(byte));
-  memset(s->registers, 0, 17 * sizeof(word));
+  memset(s->registers, 0, NOREGS * sizeof(word));
 }
 
 void swap_endian(word *x) {
@@ -25,12 +26,21 @@ void swap_endian(word *x) {
        (*x<<24);
 }
 
+word getWord(byte *memory, int n){
+
+  int start = n * sizeof(word);
+  return (memory[start] << 24)     |
+         (memory[start + 1] << 16) |   
+         (memory[start + 2] << 8)  |
+         (memory[start + 3]);
+}
+
 void load_binary(state_t *s, FILE *fp) {
-  fread(&s->memory, 1, MEMSIZE, fp);
+  fread(&s->memory, sizeof(byte), MEMSIZE, fp);
   
   word *words = (word *) s->memory;
 
-  for (int i = 0; i < MEMSIZE / 4; i++) {
+  for (int i = 0; i < MEMSIZE / sizeof(word); i++) {
     swap_endian(&words[i]);
   }
 }
@@ -40,7 +50,8 @@ int main(int argc, char **argv) {
   printf("Trying to open file %s\n", argv[1]);
 
   FILE *fp;
-  if ((fp = fopen(argv[1], "rb")) == NULL) {
+  fp = fopen(argv[1], "rb");
+  if (fp  == NULL) {
     perror("Error opening file!\n");
     exit(EXIT_FAILURE); 
   }
@@ -50,10 +61,13 @@ int main(int argc, char **argv) {
   load_binary(&s, fp);
   
   int i = 0;
-  while (s.memory[i] != 0) {
-    printf("s.memory[%d]: %x %p\n", i, s.memory[i], (void *) &s.memory[i]);
+  word curr = getWord(s.memory, i);
+  while (curr != 0) {
+    printf("s.memory[%d]: %x %p\n", i, curr, (void *) &curr);
     i++;
+    curr = getWord(s.memory, i);
   }
 
   return EXIT_SUCCESS;
+
 }
