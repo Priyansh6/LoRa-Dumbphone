ac2f6c9297aafa2015c4feaa5c965f46165b5199
Om Singh <os221@ic.ac.uk>
2022-06-06 21:39:58 +0100

    branch.h Commenting

--- src/branch.h
+++ src/branch.h
@@ -3,6 +3,12 @@
 
 #include "utilities.h"
 
+/*
+Usage:
+execute_B(instruction_t instruction, state_t *s)
+
+Executes a branch instruction. Increments the PC register by the offset.
+*/
 void execute_B(instruction_t instruction, state_t *s);
 
 #endif
