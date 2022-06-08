2fce2ecf273761d72ec4902969fd13186dde333b
Om Singh <os221@ic.ac.uk>
2022-06-08 15:51:41 +0100

    adding the free pointer to the token struct and tokeniser fullt working

--- src/assembler/tokeniser.c
+++ src/assembler/tokeniser.c
@@ -9,17 +9,26 @@
 void name(token_t *token, char *str_pointer, char *sub_token){\
   token->format = code;
 
+#define SET_IN_TOKEN_LAST(space_name, struct_name)\
+if(strlen(str_pointer) == 0){\
+  token->format = INV_F;\
+  return;\
+} else {\
+  token->contents_f.struct_name.space_name = str_pointer; \
+} 
+
 #define SET_IN_TOKEN(space_name, struct_name)\
 if(sub_token == NULL){\
   token->format = INV_F;\
   return;\
 } else {\
   token->contents_f.struct_name.space_name = sub_token; \
-} \
-sub_token = strtok_r(str_pointer, ",", &str_pointer);
+}
+
+#define GET_NEXT_TOKEN sub_token = strtok_r(NULL, ",", &str_pointer);
 
 #define  GOTO_TYPE_FUNC(str_case ,dest)\
-if (strcmp(str_case, sub_token)) { \
+if (strcmp(str_case, sub_token) == 0) { \
   \
   dest(&token, str_pointer, sub_token);\
   return token;\
@@ -27,83 +36,77 @@ if (strcmp(str_case, sub_token)) { \
 
 FUNCTION_START(tokenise_comp ,DP_COMP_F)
   SET_IN_TOKEN(opcode, dp_comp_f)
+  GET_NEXT_TOKEN 
   SET_IN_TOKEN(rd, dp_comp_f)
+  GET_NEXT_TOKEN 
   SET_IN_TOKEN(rn, dp_comp_f)
-  SET_IN_TOKEN(operand2, dp_comp_f)
+  SET_IN_TOKEN_LAST(operand2, dp_comp_f)
 }
 
 FUNCTION_START(tokenise_mov, DP_MOV_F) 
-  sub_token = strtok_r(str_pointer, ",", &str_pointer);
-
+  GET_NEXT_TOKEN
   SET_IN_TOKEN(rd, dp_mov_f)
-  SET_IN_TOKEN(operand2, dp_mov_f)
-
+  SET_IN_TOKEN_LAST(operand2, dp_mov_f)
 }
 
 FUNCTION_START(tokenise_ncomp, DP_NCOMP_F )
-
   SET_IN_TOKEN(opcode, dp_ncomp_f)
+  GET_NEXT_TOKEN  
   SET_IN_TOKEN(rn, dp_ncomp_f)
-  SET_IN_TOKEN(operand2, dp_ncomp_f)
-
+  SET_IN_TOKEN_LAST(operand2, dp_ncomp_f)
 }
 
 FUNCTION_START(tokenise_m , M_F)
-  sub_token = strtok_r(str_pointer, ",", &str_pointer);
-
+  GET_NEXT_TOKEN
   SET_IN_TOKEN(rd, m_f)
+  GET_NEXT_TOKEN  
   SET_IN_TOKEN(rm, m_f)
-  SET_IN_TOKEN(rs, m_f)
-
+  SET_IN_TOKEN_LAST(rs, m_f)
 }
 
 FUNCTION_START(tokenise_ma, MA_F) 
-  sub_token = strtok_r(str_pointer, ",", &str_pointer);
-
-
+  GET_NEXT_TOKEN
   SET_IN_TOKEN(rd, ma_f)
+  GET_NEXT_TOKEN
   SET_IN_TOKEN(rm, ma_f)
+  GET_NEXT_TOKEN
   SET_IN_TOKEN(rs, ma_f)
-  SET_IN_TOKEN(rn, ma_f)
-
+  SET_IN_TOKEN_LAST(rn, ma_f)
 }
 
 FUNCTION_START(tokenise_sdt, SDT_F)
-
   SET_IN_TOKEN(expr, sdt_f)
+  GET_NEXT_TOKEN
   SET_IN_TOKEN(rd, sdt_f)
-  SET_IN_TOKEN(addr, sdt_f)
-
-
+  SET_IN_TOKEN_LAST(addr, sdt_f)
 }
 
 
 FUNCTION_START(tokenise_b, B_F)
-
   SET_IN_TOKEN(cond, b_f)
-  SET_IN_TOKEN(offset, b_f)
-
+  SET_IN_TOKEN_LAST(offset, b_f)
 }
 
 FUNCTION_START(tokenise_andeq, ANDEQ_F)
 }
 
 FUNCTION_START(tokenise_lsl, LSL_F) 
-  sub_token = strtok_r(str_pointer, ",", &str_pointer);
-
+  GET_NEXT_TOKEN
   SET_IN_TOKEN(rn, lsl_f)
-  SET_IN_TOKEN(expr, lsl_f)
-
+  SET_IN_TOKEN_LAST(expr, lsl_f)
 }
 
-token_t tokenise_line(char *line){
+token_t tokenise_line(const char *line){
   token_t token; 
-  
-  char *str_pointer, *sub_token;
-  str_pointer = malloc(strlen(line) + 1);
-  strcpy(str_pointer, line);
+
+  size_t line_length = strlen(line);
  
-  sub_token = strtok_r(str_pointer, " ", &str_pointer);
+  char *str_holder, *str_pointer, *sub_token;
+  str_holder = malloc(line_length + 1);
+  strncpy(str_holder, line, line_length);
+  token.free_pointer = str_holder; 
+ 
+  sub_token = strtok_r(str_holder, " ", &str_pointer);
 
   GOTO_TYPE_FUNC("and", tokenise_comp )
   GOTO_TYPE_FUNC("eor", tokenise_comp )
@@ -144,6 +147,8 @@ int main(void){
   printf("1 %s\n", t.contents_f.sdt_f.expr);
   printf("2 %s\n", t.contents_f.sdt_f.rd);
   printf("3 %s\n", t.contents_f.sdt_f.addr);
+
+  free(t.free_pointer);
 
 return 0;
 }
---
+++ src/assembler/utilities.h
@@ -0,0 +1,147 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#define MEMSIZE 64 * 1024
+#define NOREGS 17
+#define PC 15
+#define CPSR 16
+#define GPIOSIZE 12
+#define GPIO_CLEAR 0x20200028
+#define GPIO_SET 0x2020001C
+#define GPIO_START 0x20200000
+#define GPIO_END 0x2020002C
+
+#define BIT_MASK(x, shift, mask) ((x >> shift) & mask)
+
+#include <stdint.h>
+
+typedef uint8_t byte;
+typedef uint16_t address;
+typedef uint32_t word;
+
+typedef struct state {
+  byte memory[MEMSIZE];
+  word registers[NOREGS];
+  byte gpio_memory[GPIOSIZE];
+  word gpio_clear;
+  word gpio_set;
+} state_t;
+
+enum InstructionType { DP, M, SDT, B, T };
+
+typedef struct instruction {
+  enum InstructionType type;
+  byte cond;
+  union contents_u {
+    struct dp_s { 
+      byte i, opcode, s, rn, rd; 
+      uint16_t operand2;  
+    } dp;  
+    struct m_s { 
+      byte a, s, rd, rn, rs, rm; 
+    } m;  
+    struct sdt_s { 
+      byte i, p, u, l, rn, rd; 
+      uint16_t offset; 
+    } sdt;  
+    struct b_s { 
+      word offset; 
+    } b;  
+  } contents;
+} instruction_t;
+
+enum DPType { COMP, MOV, NCOMP };
+enum MType { SYN, ACCSYN };
+
+enum InstructionFormat {DP_COMP_F, DP_MOV_F, DP_NCOMP_F, M_F, MA_F, SDT_F, B_F, ANDEQ_F, LSL_F, INV_F};
+
+typedef struct token {
+  enum InstructionFormat format;
+  union contents_f_u {
+    struct dp_comp_f_s {
+      char *opcode, *rd, *rn, *operand2;
+    } dp_comp_f;
+    struct dp_mov_f_s {
+      char *rd, *operand2;
+    } dp_mov_f;
+    struct dp_ncomp_f_s {
+      char *opcode, *rn, *operand2;
+    } dp_ncomp_f;
+    struct m_f_s {
+      char *rd, *rm, *rs;
+    } m_f;
+    struct ma_f_s {
+      char *rd, *rm, *rs, *rn; 
+    } ma_f;
+    struct sdt_f_s {
+      char *expr, *rd, *addr;
+    } sdt_f;
+    struct b_f_s {
+      char *cond, *offset;
+    } b_f;
+    struct lsl_f_s {
+      char *rn, *expr;
+    } lsl_f;
+  } contents_f;
+} token_t;
+
+/*
+Usage:
+word shifted_rm(uint16_t shift_rm, state_t* s);
+<<<<<<< HEAD
+=======
+
+Computes shifted register based on operand2 and offset fields in data processing and single data transfer instructions respectively
+*/
+word shifted_rm(uint16_t shift_rm, state_t* s);
+
+/*
+Usage:
+word rotate_shift_right(word x, word shift_amount);
+
+Computes the value of x rotated right by shift_amount bits
+*/
+word rotate_shift_right(word x, word shift_amount);
+
+
+/*
+Usage:
+pprint_instruction_t(instruction_t instruction);
+
+Pretty prints the instruction type with all fields. Useful for debugging.
+*/
+void pprint_instruction_t(instruction_t instruction);
+
+/*
+Usage:
+pprint_state_t(state_t s);
+
+Pretty prints the state. Outputs all registers and non-zero memory.
+*/
+void pprint_state_t(state_t s);
+
+/*
+Usage:
+get_word(byte *memory, int n);
+
+Gets a word from memory[n] and puts in in big endian form for processing.
+*/
+word get_word(byte *memory, int n);
+
+/*
+Usage:
+get_word_raw(byte *memory, int n);
+
+Gets a word from memory[n] as it is laid out in memory.
+*/
+word get_word_raw(byte *memory, int n);
+
+/*
+Usage:
+set_word(byte *memory, int addr, word w);
+
+Sets the next four bytes in memory from addr to the four bytes comprising word.
+*/
+void set_word(byte *memory, int addr, word w);
+
+#endif
