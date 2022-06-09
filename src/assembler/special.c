c9611e9e133d513c3cea43d9a758c430d569bb6e
Priyansh Mahajan <pm521@ic.ac.uk>
2022-06-09 15:18:27 +0100

    Reformed special

--- src/assembler/special.c
+++ src/assembler/special.c
@@ -6,39 +6,17 @@
 #include "tokeniser.h"
 
 #define LSL t.contents_f.lsl_f
+#define MOV new_t.contents_f.dp_mov_f
 
 word assemble_ANDEQ(token_t t) {
   return 0;
 }
 
 word assemble_LSL(token_t t) {
-  char *start_line = "mov ";
-  char *mid_line = ",lsl ";
+  token_t new_t;
+  new_t.format = DP_MOV_F;
+  MOV.rd = LSL.rn;
+  MOV.operand2 = { .values_oper_t.sh_reg = { .rm = LSL.rn, .shifted_vals_t.immediate = LSL.expr } };
 
-  int start_len = strlen(start_line);
-  int mid_len = strlen(mid_line);
-  int rn_len = strlen(LSL.rn);
-  int expr_len = strlen(LSL.expr);
-  int total_len = 2 + start_len + 2 * rn_len + mid_len + expr_len;
-
-  char *new_line = malloc(total_len + 1);
-  char *index = new_line;
-  memcpy(index, start_line, start_len);
-  index += start_len;
-  memcpy(index, LSL.rn, rn_len);
-  index += rn_len;
-  new_line[index - new_line] = ',';
-  index++;
-  new_line[index - new_line] = ' ';
-  index++;
-  memcpy(index, LSL.rn, rn_len);
-  index += rn_len;
-  memcpy(index, mid_line, mid_len);
-  index += mid_len;
-  memcpy(index, LSL.expr, expr_len);
-  new_line[total_len] = '\0';
-
-  free(t.free_pointer);
-
-  return assemble_DP(tokenise_line(new_line));
+  return assemble_DP(new_t);
 }
