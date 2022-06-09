2c4202170068d6bedf3527742e1e88c79b3cf20a
agm21 <agm21@ic.ac.uk>
2022-06-09 13:29:21 +0100

    Removed type conversion from multiply

--- src/assembler/multiply.c
+++ src/assembler/multiply.c
@@ -6,23 +6,22 @@
 #define MA t.contents_f.ma_f
 #define SET(r, s) result |= (r << s)
 #define SET_FILLER result |= 9 << 4
-#define CONVERT(r) strtoul(r + 1, NULL, 10)
 
 word assemble_M(token_t t) {
   word result = 14 << 28; // Setting condition code to 14
  
   if (t.format == M_F) {
-    SET(CONVERT(M.rd), 16);
-    SET(CONVERT(M.rs), 8);
+    SET(M.rd, 16);
+    SET(M.rs, 8);
     SET_FILLER;
-    SET(CONVERT(M.rm), 0);
+    SET(M.rm, 0);
   } else {
     result |= 1 << 21;
-    SET(CONVERT(MA.rd), 16);
-    SET(CONVERT(MA.rn), 12);
-    SET(CONVERT(MA.rs), 8);
+    SET(MA.r, 16);
+    SET(MA.r, 12);
+    SET(MA.r, 8);
     SET_FILLER;
-    SET(CONVERT(MA.rm), 0);
+    SET(MA.r, 0);
   }
 
   return result;
