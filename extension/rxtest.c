68b5028617da961ac4e430c8ab46d39be1ef3c0d
Hamish McCreanor <hwm21@ic.ac.uk>
2022-06-18 20:39:42 +0100

    modified lora receiving test

--- extension/rxtest.c
+++ extension/rxtest.c
@@ -1,10 +1,13 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <wiringPi.h>
 #include <wiringSerial.h>
 
 int main() {
   int fd;
+  int e;
   if ((fd = serialOpen("/dev/ttyS0", 9600)) < 0) {
     perror("Unable to open serial!");
     exit(EXIT_FAILURE);
@@ -15,10 +18,11 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-  for (int i = 0; i < 180; i++) {
-    while (serialDataAvail(fd)) {
+  for (int i = 0; i < 240; i++) {
+    while ((e = serialDataAvail(fd)) > 0) {
       printf("%c", serialGetchar(fd));
     }
+    printf("\nerrno: %s\n", strerror(errno));
     //serialFlush(fd);
     fflush(stdout);
     delay(500);
