978b32d60406adb08b01c883e976c090b67be66c
Adam Maltby <agm21@ic.ac.uk>
2022-06-20 13:04:24 +0100

    Added windows to interface

--- extension/interface.c
+++ extension/interface.c
@@ -7,19 +7,18 @@
 #include "keypad.h"
 #include "utilities.h"
 
+WINDOW *create_newwin(int height, int width, int starty, int startx);
+
 int main(void) {
   wiringPiSetup();
   init_keypad();
 
   initscr();
-
-  // addstr("\n\n\n\n      WELCOME MESSAGE     \n\n\n\n");
-  // refresh();
-
-  // addstr("      HELP MESSAGE        \n\n\n\n");
-  // refresh();
+  refresh();
 
   bool quit = false;
+
+  enum Win {CHAT_WIN = 0, IN_WIN = 1};
 
   /* 1. Receive from specific sender
      2. Receive from all
@@ -27,26 +26,59 @@ int main(void) {
      4. Chat
      */
 
-  int MAX_X, MAX_Y;
+  int MAX_HEIGHT, MAX_WIDTH;
+  int chat_start_x, chat_start_y, input_start_x, input_start_y;
+  float chat_scale;
 
-  getmaxyx(stdscr, MAX_X, MAX_Y);
+  getmaxyx(stdscr, MAX_HEIGHT, MAX_WIDTH);
+  chat_scale = 0.85;
 
-  printw("x: %d | y: %d", MAX_X, MAX_Y);
-  refresh();
+  WINDOW *chat_win = create_newwin(
+    MAX_HEIGHT * chat_scale,
+    MAX_WIDTH,
+    0,
+    0);
 
-  /* Window creation */
-  /* utc00:00:00|01/01/2022|user1> Blah blah blah*/
+  chat_start_y = 1;
+  chat_start_x = 1;
+
+  WINDOW *input_win = create_newwin(
+    MAX_HEIGHT * (1 - chat_scale),
+    MAX_WIDTH,
+    MAX_HEIGHT * chat_scale,
+    0);
+
+  input_start_y = (MAX_HEIGHT * chat_scale) + 1;
+  input_start_x = 1;  
+
+  // move(input_start_y, input_start_x);
+  // printw("adam@|utc+1|22:58:11|01-10-2002|: Hamish is cool");
+  // refresh();
+
+  int cur_y, cur_x;
 
   while(!quit) {
+    getyx(cur_y, cur_x);
+
+    
     break;
   }
-
-  addstr("\npress any key to exit...");
-  refresh();
 
   getch();
 
   endwin();
 
   return 0;
+}
+
+WINDOW *create_newwin(int height, int width, int starty, int startx)
+{	WINDOW *local_win;
+
+	local_win = newwin(height, width, starty, startx);
+	box(local_win, 0 , 0);		/* 0, 0 gives default characters 
+					 * for the vertical and horizontal
+					 * lines			*/
+	wrefresh(local_win);		/* Show that box 		*/
+
+	return local_win;
 }
