8b3e4d3c5740611319c6bfb1d15771f52c842ff1
Hamish McCreanor <hwm21@ic.ac.uk>
2022-06-20 14:47:57 +0100

    added message priority queue

--- extension/lora.c
+++ extension/lora.c
@@ -8,6 +8,76 @@
 #define SERIAL_PATH "/dev/ttyS0"
 #define BAUD_RATE 9600
 
+pq_t *alloc_pq() {
+  pq_t *pq = (pq_t *) malloc(sizeof(pq_t));
+  pq->head = NULL;
+  return pq;
+}
+
+pq_node_t *alloc_pq_node(message_t message) {
+  pq_node_t *pqn = (pq_node_t *) malloc(sizeof(pq_node_t));
+  pqn->message = message;
+  pqn->next = NULL;
+  return pqn;
+}
+
+void free_pq_node(pq_node_t *pqn) {
+  if (pqn->next == NULL) {
+    return;
+  }
+  free_pq_node(pqn->next);
+}
+
+void free_pq(pq_t *pq) {
+  free_pq_node(pq->head);
+}
+
+void print_pq_node(pq_node_t *pqn) {
+  printf("Sender: %s\n", pqn->message.sender);
+  printf("Contents: %s\n", pqn->message.contents);
+  printf("Time: %d\n", pqn->message.time);
+  
+  if (pqn->next != NULL) {
+    print_pq_node(pqn->next);
+  }
+  printf("\n\n");
+}
+
+void print_pq(pq_t *pq) {
+  if (pq->head != NULL) {
+    print_pq_node(pq->head);
+  }
+}
+
+void add_to_pq(pq_t *pq, message_t message) {
+  pq_node_t *node = alloc_pq_node(message);
+  pq_node_t *curr = pq->head;
+
+  if (curr == NULL || node->message.time < curr->message.time) {
+    pq->head = node;
+    node->next = curr;
+    return;
+  }
+
+  pq_node_t *prev = NULL;
+
+  while (curr != NULL && node->message.time > curr->message.time) {
+    prev = curr;
+    curr = curr->next;
+  }
+
+  node->next = curr;
+  prev->next = node;
+}
+
+message_t pop_from_pq(pq_t *pq) {
+  pq_node_t *pqn = pq->head;
+  message_t message = pqn->message;
+  pq->head = pqn->next;
+  free(pqn);
+  return message;
+}
+
 int init_lora() {
   int fd;
   if ((fd = serialOpen(SERIAL_PATH, BAUD_RATE)) < 0) {
@@ -37,3 +107,27 @@ bool receive_message(int fd, message_t *message) {
   return received;
 }
 
+/*
+int main() {
+  message_t m1 = {"m1", "sender1", 2};
+  message_t m2 = {"m2", "sender2", 7};
+  message_t m3 = {"m3", "sender3", 4};
+  message_t m4 = {"m4", "sender1", 1};
+
+  // ordering is t4, t1, t3, t2
+
+  pq_t *pq = alloc_pq();
+  add_to_pq(pq, m1);
+  print_pq(pq);
+  add_to_pq(pq, m2);
+  print_pq(pq);
+  add_to_pq(pq, m3);
+  print_pq(pq);
+  add_to_pq(pq, m4);
+  print_pq(pq);
+
+  message_t q1 = pop_from_pq(pq);
+  printf("%s\n", q1.contents);
+  free_pq(pq);
+}
+*/
