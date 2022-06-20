77e162674aaf609554e30ebc5cd4148b0df12683
Hamish McCreanor <hwm21@ic.ac.uk>
2022-06-20 19:15:45 +0100

    updated tx and rx test files

--- extension/txtest.c
+++ extension/txtest.c
@@ -15,13 +15,39 @@ int main() {
     exit(EXIT_FAILURE);
   }
 
-  char str[4];
-  str[3] = '\0';
+  serialPrintf(fd, "%s", "hamish@");
+  serialPutchar(fd, 0);
+  serialPutchar(fd, 0);
+  serialPutchar(fd, 3); // 60
+  serialPutchar(fd, 0xC);
+  serialPrintf(fd, "%s", "a bog standard message");
+  serialPutchar(fd, '\0');
+  printf("sent msg 1\n");
+
+  delay(2000);
+
+
+  serialPrintf(fd, "%s", "priyansh@");
+  serialPutchar(fd, 0);
+  serialPutchar(fd, 1); // 290
+  serialPutchar(fd, 2); // 
+  serialPutchar(fd, 0x2);
+  serialPrintf(fd, "old rosie");
+  serialPutchar(fd, '\0');
+
+  printf("sent msg 2\n");
+
+  /*
+  char str[5];
+  str[3] = '\n';
+  str[4] = '\0';
   for (int i = 0; i < 180; i++) {
     sprintf(str, "%d", i);
+    printf("%s\n", str);
     serialPrintf(fd, "%s", str);
     delay(1000);
   }
+  */
 
   printf("Finished sending data\n");
 
